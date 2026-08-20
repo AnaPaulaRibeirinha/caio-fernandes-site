947f9f1ac77a1917473e4f97f1eb8d37ba35effb
ana paula melo <anahoran.tommo@gmail.com>
2026-08-19 22:57:08 -0300

    Ajustando o faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,24 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::query()->updateOrCreate(
+            ['email' => 'admin@seudominio.com'],
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make('SUA_SENHA_FORTE'),
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            HomeContentSeeder::class,
         ]);
     }
 }
